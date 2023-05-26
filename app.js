407c78da42d75d578eb1070f25458f548e82f79f
felix1805 <felix1805@gmail.com>
2023-05-26 17:54:50 -0400

    completed app.js, linked all styling with colors.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ xLetterSVG.style.fill = colorPalettes[paletteIndex][xLetterIndex].fill;
 
 allPills.forEach((pill, i) =>
   pill.style.backgroundColor = colorPalettes[paletteIndex][i].fill);
-  
+
 allHiddenPills.forEach(hiddenPill =>
   hiddenPill.style.backgroundColor = colorPalettes[paletteIndex][socialFanIndex].fill)
 
@@ -49,7 +49,90 @@ const reverse = () => {
   }
 };
 
+const addTheme = (
+  bodyBackgroundColor,
+  strokeWidth,
+  svgFill,
+  opacity,
+  lineColor,
+  borderRadius,
+  boxBackgroundColor,
+  pillBackgroundColor
+) => {
+  body.style.backgroundColor = bodyBackgroundColor
+  xLetterPath.style.strokeWidth = strokeWidth
+  xLetterPath.style.stroke = lineColor || colorPalettes[paletteIndex][xLetterIndex].altStroke
+  xLetterSVG.style.fill = svgFill || colorPalettes[paletteIndex][xLetterIndex].fill
+  xLetterSVG.style.opacity = opacity
+  xBox.style.backgroundColor = boxBackgroundColor || colorPalettes[paletteIndex][xLetterIndex].fill
+  iconPath.style.stroke = lineColor || colorPalettes[paletteIndex][rotateIconIndex].altStroke
+  iconPath.style.strokeWidth = strokeWidth
 
+  allBoxes.forEach((box, i) =>
+    box.style.backgroundColor = boxBackgroundColor || colorPalettes[paletteIndex][i].fill
+  )
+
+  allPills.forEach((pill, i) => {
+    pill.style.opacity = opacity
+    pill.style.backgroundColor = pillBackgroundColor || colorPalettes[paletteIndex][i].fill
+    pill.style.borderWidth = strokeWidth
+    pill.style.borderColor = lineColor || colorPalettes[paletteIndex][i].altStroke
+    pill.style.borderBlockStyle = 'solid'
+    pill.style.borderRadius = borderRadius
+
+  })
+
+  allHiddenPills.forEach(hiddenPill => {
+    hiddenPill.style.opacity = opacity
+    hiddenPill.style.borderWidth = strokeWidth
+    hiddenPill.style.backgroundColor = pillBackgroundColor || colorPalettes[paletteIndex][socialFanIndex].fill
+    hiddenPill.style.borderColor = lineColor || colorPalettes[paletteIndex][socialFanIndex].altStroke
+    hiddenPill.style.borderRadius = borderRadius
+  })
+
+  allArrows.forEach(arrow => {
+    arrow.style.borderBlockStyle = 'solid'
+    arrow.style.borderColor = lineColor
+    arrow.style.borderWidth = '0 ' + strokeWidth + ' ' + strokeWidth + ' 0'
+    arrow.style.opacity = opacity
+  })
+}
+
+const moveSlider = () => {
+  const sliderInput = document.querySelector('#slider-input')
+  const sliderValue = sliderInput.value
+
+  if (sliderValue == 0) {
+    // bodyBackgroundColor,strokeWidth,svgFill,opacity,lineColor,borderRadius,boxBackgroundColor,pillBackgroundColor
+    addTheme('white', '12px', null, 1, 'rgb(38, 38, 38', '100px', 'white', null)
+  }
+  if (sliderValue > 1 && sliderValue <= 3) {
+    addTheme('white', '2px', 'white', 0.5, 'null', '75px', null, 'white')
+  }
+  if (sliderValue > 4 && sliderValue <= 6) {
+    addTheme('white', '1px', 'white', 0.5, 'null', '90px', null, 'white')
+  }
+  
+  if (sliderValue > 7 && sliderValue < 9) {
+    addTheme('white', '2px', 'white', 0.5, 'rgb(38, 38, 38)', '50px', null, 'white')
+  }
+  if (sliderValue == 10) {
+    addTheme('rgb(38, 38, 38)', '12px', 'white', 1, 'black', 0, 'transparent', 'white')
+  }
+};
+
+const changePalette = () => {
+  xLetterPath.classList.add('pulse')
+  if (paletteIndex >= 2) {
+    paletteIndex = 0
+  } else {
+    paletteIndex++
+  }
+  moveSlider()
+  setTimeout(() => xLetterPath.classList.remove('pule', 500))
+}
 
 expandPill.addEventListener('click', expand);
 reversePill.addEventListener('click', reverse);
+sliderInput.addEventListener('input', moveSlider);
+xBox.addEventListener('click', changePalette)
